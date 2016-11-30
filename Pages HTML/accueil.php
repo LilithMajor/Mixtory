b16049caf571e1e77c18b364bdca0e8f9a9a0e5a
unknown <grammont@B-1-18-15.iutparis.local>
2016-11-30 09:36:43 +0100

    Test

---
+++ Pages HTML/accueil.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Mixtory</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        
+        <!-- mobile specific info ... mobile specify windows size to 800/900 whereas device-width is less in general, resulting in small texte etc.- those meta correct this... -->
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no" />
+        <!-- end mobile specific info -->
+        
+        
+    <link rel="stylesheet" type="text/css" href="./joli.css" />
+	</head>
+	<body>
+		
+		<div id="globale">	
+			
+			<header> <!-- balise html5 equivalant à <div id="header"> -->
+				<button id="bouton">Horreur</button>			
+				<button id="bouton1">Comédie</button>
+				<button id="bouton2">Science-Fiction</button>
+			</header>
+			<div id = "ex-story">Voici les anciennes histoires
+			<?php echo "Test PHP TMTC"; ?>
+			</div> <!--Bouton-->
+			<!-- <div id = "pre-texte"><article>Me voici toute puissante je suis une nébuleuse</article></div> -->
+			<!-- <div id = "nbr_ligne_restante">Il reste %f lignes !</div> -->
+			<form method = "post" action = "cible.php">
+			<div id = "texte"><textarea name = "texte">Rentrez votre texte</textarea></div>
+			<div id = "nbr_ligne">Choissisez le nombre de lignes : <input type = "text" name = "nbr_lignes"/></div>
+			<div id = "adresse-mail"><input type = "text" name = "email" value = "Adresse Mail"/></div>
+			<input type= "submit" value = "Go !"/>
+			</form>
+			<footer>
+								Nous contacter : <a href="mailto:g.grammont@hotmail.fr">Mail</a>
+			</footer>
+		</div> <!-- fin id="globale"-->
+	</body>
+</html>
