2c5c2aa33418b1fa5560e68aa43162ae3e93b302
unknown <grammont@B-1-18-15.iutparis.local>
2016-11-30 10:24:39 +0100

    suppression cible test php accueil

--- Pages HTML/accueil.php
+++ Pages HTML/accueil.php
@@ -21,17 +21,28 @@
 				<button id="bouton1">Comédie</button>
 				<button id="bouton2">Science-Fiction</button>
 			</header>
+			
+			<?php 
+			$nbr_lignes_restantes = $POST_['nbr_ligne'] - $POST_['text'][size];
+			if (isset($_POST['texte'])){ ?>
+				<form method = "post" action = "accueil.php">
+					<div id = "texte"><textarea name = "texte">Rentrez votre texte</textarea></div>
+					<div id = "nbr_ligne">Choissisez le nombre de lignes : <input type = "text" name = "nbr_lignes"/></div>
+					<div id = "adresse-mail"><input type = "text" name = "email" value = "Adresse Mail"/></div>
+				<input type= "submit" value = "Go !"/>
+				</form>
+				<?php 
+			}
+			else{ ?>
+				<div id = "pre-texte"><article><?php echo $POST_['texte']?></article></div>
+				<div id = "nbr_ligne_restante">Il reste <?php echo $nbr_ligne_restantes?> lignes !</div>
+			<?php
+			}
+			?>
+
 			<div id = "ex-story">Voici les anciennes histoires
-			<?php echo "Test PHP TMTC"; ?>
 			</div> <!--Bouton-->
-			<!-- <div id = "pre-texte"><article>Me voici toute puissante je suis une nébuleuse</article></div> -->
-			<!-- <div id = "nbr_ligne_restante">Il reste %f lignes !</div> -->
-			<form method = "post" action = "cible.php">
-			<div id = "texte"><textarea name = "texte">Rentrez votre texte</textarea></div>
-			<div id = "nbr_ligne">Choissisez le nombre de lignes : <input type = "text" name = "nbr_lignes"/></div>
-			<div id = "adresse-mail"><input type = "text" name = "email" value = "Adresse Mail"/></div>
-			<input type= "submit" value = "Go !"/>
-			</form>
+			
 			<footer>
 								Nous contacter : <a href="mailto:g.grammont@hotmail.fr">Mail</a>
 			</footer>
