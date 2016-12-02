5db47c9ff544c6c81b30c7f954b449ba7178161e
Gustave <students@srv.elwinar.com>
2016-12-02 21:55:01 +0100

    Je comprends les routes omg

--- mixtory/src/AppBundle/Controller/RegisterController.php
+++ mixtory/src/AppBundle/Controller/RegisterController.php
@@ -5,28 +5,16 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    
+    /**
+     * @Route("/", name="homepage")
+     */
     public function indexAction(Request $request)
     {
-		return  $this->render('new_story.html.twig');
-	}
-
-	public function newlineAction(Request $request)
-	{
 		$pretexte = $request->request->get('texte');
 		return $this->render('new_line.html.twig', array('pretexte' => $pretexte)); 
 	}
-	
 
 }
-
-/* /
-/new
-/register
-/newLine
-/registerLine
-/show?id=1 */
