1cba363228e304bca6a6a7284b423f324eaaf857
Gustave <students@srv.elwinar.com>
2016-12-03 12:49:55 +0100

    On tient quelquechose la

--- mixtory/src/AppBundle/Controller/DefaultController.php
+++ mixtory/src/AppBundle/Controller/DefaultController.php
@@ -12,18 +12,23 @@ class DefaultController extends Controller
     
     public function indexAction(Request $request)
     {
-
 		return  $this->render('new_story.html.twig');
+    }
+    public function firstlineAction(Request $request)
+    {
+        $pretexte = $request->request->get('texte');
+        $id = $request->request->get('nbr_auteurs');
+        if($id == 1){
+            return $this->render('last_autor.html.twig', array('pretexte' =>$pretexte));
+        }
+        else{
+            return $this->render('new_line.html.twig', array('pretexte' =>$pretexte,'id' => $id));
+        }
     }
 
 	public function newlineAction(Request $request, $id)
     {
-        if($id == null){
-            $id = $request->request->get('nbr_auteurs');
-        }
-        else{
-            $id--;
-        }
+        $id--;
         $pretexte = $request->request->get('texte');
         if($id == 1){
             return $this->render('last_autor.html.twig', array('pretexte' =>$pretexte));  
@@ -31,9 +36,10 @@ class DefaultController extends Controller
         else{
             return $this->render('new_line.html.twig', array('pretexte' => $pretexte,'id' => $id));
         } 
-	}
-	
-
+    }
+    public function envoiAction(Request $request){
+        return new Response("Test");
+    }
 }
 
 
